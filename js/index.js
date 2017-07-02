694717e26f53d7eb226d07fb794fa4f2fcdfc95e
yatso <yathso@gmail.com>
2017-07-02 15:23:13 -0700

    Revert "v2 - Return handle"
    This reverts commit c968013d829b9745d5eb7bf8ebf706ad6744cac4.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,8 @@
 var app = angular.module('githubFollowersApp', []);
 
-app.controller('appCtrl', function ($scope, $http) {
-    $scope.username = "yatso";
+app.controller('appCtrl', function ($scope) {
+    $scope.username = 'yatso';
     $scope.fetchAll = function () {
-        $scope.fetchUserInfo();
-    }
-    $scope.fetchUserInfo = function () {
-        $http({
-            method: "GET",
-            url: "https://api.github.com/users/" + $scope.username
-        }).then(
-            function (result) {
-                console.log(result);
-                $scope.userObj = result.data;
-                console.log($scope.userObj);
-            },
-            function (error) {
-                console.log(error);
-            }
-        );
+        console.log('this function will fetch github api results for ' + $scope.username);
     };
 });
