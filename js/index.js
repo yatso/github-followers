4e5559c25548df17c98441cd1307fa0a7d6f9ac4
yatso <yathso@gmail.com>
2017-07-02 14:11:20 -0700

    v1 - Search input
    User can search by target GitHub username.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
-var app = angular.module('yourApp', []);
+var app = angular.module('githubFollowersApp', []);
 
-app.controller('appCtrl', function($scope) {
-    
+app.controller('appCtrl', function ($scope) {
+    $scope.username = 'yatso';
+    $scope.fetchAll = function () {
+        console.log('this function will fetch github api results for ' + $scope.username);
+    };
 });
